8c78c43535e92910b4bc623bb13f0119e143cecc
JacK9004 <kjushkin@gmail.com>
2024-06-19 13:39:34 +0900

    feat: configure product schema model

---
+++ src/libs/enums/product.enum.ts
@@ -0,0 +1,25 @@
+export enum ProductSize {
+ 
+  NORMAL = "NORMAL",
+  LARGE = "LARGE",
+  SET = "SET",
+}
+
+export enum ProductVolume {
+ HOT = "HOT",
+ICE = "ICE",
+}
+
+export enum ProductStatus {
+  PAUSE = "PAUSE",
+  PROCESS = "PROCESS",
+  DELETE = "DELETE",
+}
+
+export enum ProductCollection {
+  COFFEE = "COFFEE",
+  TEA = "TEA",
+ JUCE = "JUCE",
+  DESERT = "DESERT",
+  SET = "SET",
+}
