9d3ef9fbca0cd76384efc6405c494ad115d06d1c
JacK9004 <kjushkin@gmail.com>
2024-06-19 13:50:07 +0900

    fix: modify coffeeController  processSignup and processLogin

--- src/controllers/coffee.controller.ts
+++ src/controllers/coffee.controller.ts
@@ -3,7 +3,7 @@ import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
 import { AdminRequest, LoginInput, MemberInput } from "../libs/types/member";
 import { MemberType } from "../libs/enums/member.enum";
-import Errors, { Message } from "../libs/Errors";
+import Errors, { HttpCode, Message } from "../libs/Errors";
 
 const memberService = new MemberService();
 
@@ -47,15 +47,19 @@ coffeeController.processSignup = async (
     try {
         console.log("processSignup");
         // console.log("body", req.body);
-
+        const file = req.file;
+        if (!file)
+        throw new Errors(HttpCode.BAD_REQUEST, Message.SOMETHING_WENT_WRONG);
+      
         const newMember: MemberInput = req.body;
+        newMember.memberImage = file?.path;
         newMember.memberType = MemberType.COFFEE; 
         const result = await memberService.processSignup(newMember);
         // TODO: SESSIONS AUTHENTICATION
 
         req.session.member = result;
         req.session.save(function() {
-            res.send(result);
+            res.redirect("/admin/product/all");
         });
        
     } catch (err) {
@@ -80,7 +84,7 @@ coffeeController.processLogin = async (
         // TODO: SESSIONS AUTHENTICATION
         req.session.member = result;
         req.session.save(function() {
-            res.send(result);
+            res.redirect("/admin/product/all");
         });
 
     } catch (err) {
