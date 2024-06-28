6a39b56cbd05156f2a8c692d5bd21ecc0c7219c8
JacK9004 <kjushkin@gmail.com>
2024-06-29 04:11:46 +0900

    feat: updateMember business logic

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -71,6 +71,18 @@ public async signup(input: MemberInput): Promise<Member> {
         return result;
     }
 
+    public async updateMember(
+        member: Member,
+        input: MemberUpdateInput
+        ): Promise<Member> {
+        const memberId = shapeIntoMongooseObjectId(member._id);
+        const result = await this.memberModel
+            .findOneAndUpdate({ _id: memberId,}, input, { new: true})
+            .exec();
+        if (!result) throw new Errors(HttpCode.NOD_MODIFIED, Message.UPDATE_FAILED);
+        
+        return result;
+    }
 
 
     
