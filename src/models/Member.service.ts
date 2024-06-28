35bd793e73147beaa14b35a5e1d6402254f1e00e
JacK9004 <kjushkin@gmail.com>
2024-06-29 04:27:25 +0900

    feat: develop getTopUsers rest API

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -84,6 +84,20 @@ public async signup(input: MemberInput): Promise<Member> {
         return result;
     }
 
+    public async getTopUsers(): Promise<Member[]> {
+        const result = await this.memberModel
+            .find({
+                memberStatus: MemberStatus.ACTIVE,
+                memberPoints: { $gte: 1 }
+            })
+            .sort({ memberPoints: -1 })
+            .limit(4)
+            .exec();
+        if(!result) throw new Errors(HttpCode.NON_FOUND, Message.NO_DATA_FOUND);
+
+        return result;
+    }
+
 
     
 /**  SSR (SERVER SIDE RENDERING FOR BURAK ADMIN PROJECT )*/ 
