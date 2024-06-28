2880d740c5915c0e0cd07742af11fbc44f2d285b
JacK9004 <kjushkin@gmail.com>
2024-06-29 05:03:58 +0900

    feat: develop getProducts rest API

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,7 @@ import express from "express";
 const router = express.Router();
 import memberController from "./controllers/member.controller";
 import uploader from "./libs/utils/uploader";
+import productController from "./controllers/product.controller";
 
 /** Member **/
 router.get("/member/coffee", memberController.getCoffee);
@@ -27,10 +28,8 @@ router.get("/member/top-users", memberController.getTopUsers);
     
 
 
-
-
-
 /** Product **/
+router.get("/product/all", productController.getProducts);
 
 
 /** Order **/
