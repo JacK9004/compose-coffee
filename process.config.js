9620fbcf52e50373d8c5de75fda529acdbce5e0f
JacK9004 <kjushkin@gmail.com>
2024-10-05 00:36:23 +0900

    feat: deploy project via shell & pm2

---
+++ process.config.js
@@ -0,0 +1,18 @@
+const { watch } = require("fs-extra");
+
+module.exports = {
+    apps : [{
+      name   : "COMPOSE_COFFEE",
+      cwd: "./",
+      script : "./dist/server.js",
+      watch: false,
+      env_production: {
+         NODE_ENV: "production"
+      },
+      env_development: {
+         NODE_ENV: "development"
+      },
+      instances: 1,
+      exec_mode: "cluster"
+    }]
+  }
